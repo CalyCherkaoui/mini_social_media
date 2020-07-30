5ee1025c98914c706a6e477e037333cd0e07a6fc
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-30 07:55:33 -0500

    Refactoring html with provide

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "my Mini Social-media app"
+  end
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | my Mini Social-media app"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | my Mini Social-media app"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | my Mini Social-media app"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
