c7512f00c908a2975e2d2e4a56652df04afec6dc
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-07-31 15:30:03 -0500

    Validate the uniqueness of email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -44,4 +44,11 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{adress.inspect} should be invalid"
     end
   end
+
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
