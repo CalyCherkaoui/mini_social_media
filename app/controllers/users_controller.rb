3df1e52e9f8dbc62480b9f3601b4080d5bfa91ec
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-01 14:30:17 -0500

    Adding create action to users controller witch will handle invalid entries in the sign-up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+#   user_params method allows us to specify which pa-
+# rameters are required and which ones are permitted. In addition, passing in a
+# raw params hash as above will cause an error to be raised, so that Rails appli-
+# cations are now immune to mass assignment vulnerabilities by default.
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # sucsseful save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    # Since user_params will only be used internally by the Users controller and
+    # need not be exposed to external users via the web, we’ll make it private using
+    def user_params 
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
