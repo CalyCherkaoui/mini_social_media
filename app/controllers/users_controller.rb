29cbd1485d38d0b622e4306792e2eac5bf85b7d5
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-01 13:13:54 -0500

    Creating the sign-up dynamic page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
-  def new
-  end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
 
   # run gavatar for the given user
-  def gravatar_for(user)
+  def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
